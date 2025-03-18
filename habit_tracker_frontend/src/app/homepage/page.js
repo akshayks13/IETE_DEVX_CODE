2ad63a918380328b77137464fd0aa3db8cebafdb
Achilles80 <adithyaajay2005@gmail.com>
2025-03-18 17:08:55 +0530

    .

---
+++ habit_tracker_frontend/src/app/homepage/page.js
@@ -0,0 +1,19 @@
+import Habits from "../components/Habits";
+import Dailies from "../components/Dailies";
+import Todos from "../components/Todos";
+import Rewards from "../components/Rewards";
+import Topbar from "../components/Topbar";
+
+export default function Home() {
+  return (
+    <div className="bg-dark text-white min-h-screen px-6 py-4">
+      <Topbar />
+      <div className="grid grid-cols-4 gap-4">
+        <Habits />
+        <Dailies />
+        <Todos />
+        <Rewards />
+      </div>
+    </div>
+  );
+}
