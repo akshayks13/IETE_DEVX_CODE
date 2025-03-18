397aa43b32329a14debc0fa2853bf8847e01dcaf
adhikkesh <adhikkesh@gmail.com>
2025-03-18 18:05:19 +0530

    Pages updated

--- habit_tracker_frontend/src/app/page.js
+++ habit_tracker_frontend/src/app/page.js
@@ -3,11 +3,12 @@ import Dailies from "./components/Dailies";
 import Todos from "./components/Todos";
 import Rewards from "./components/Rewards";
 import Topbar from "./components/Topbar";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div className="bg-dark text-white min-h-screen px-6 py-4">
-      <Topbar />
+      <Navbar />
       <div className="grid grid-cols-4 gap-4">
         <Habits />
         <Dailies />
